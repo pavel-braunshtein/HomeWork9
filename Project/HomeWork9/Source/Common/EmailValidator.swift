a95b2af9111b78e595d02903d3762423c566c3d8
pavelbraun333 <pavelbraun333@gmail.com>
2024-04-02 16:19:02 +0300

    task has been completed

---
+++ Project/HomeWork9/Source/Common/EmailValidator.swift
@@ -0,0 +1,8 @@
+//
+//  EmailValidator.swift
+//  HomeWork9
+//
+//  Created by Pavel on 01.04.2024.
+//
+
+import Foundation
---
+++ Project/HomeWork9/Source/Common/PasswordValidator.swift
@@ -0,0 +1,8 @@
+//
+//  PasswordValidator.swift
+//  HomeWork9
+//
+//  Created by Pavel on 01.04.2024.
+//
+
+import Foundation
